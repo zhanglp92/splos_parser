bc83f6417ff5e83c3d20d1893c00ec20a4126708
zhanglp92 <820221185@qq.com>
2015-05-19 00:25:30 +0800

    实现define

---
+++ splos_parser-5-18-1/tools/error.c
@@ -0,0 +1,80 @@
+/*************************************************************************
+	> File Name: error.c
+
+	> Author: zhanglp
+
+	> Mail: zhanglp92@gmail.com
+
+	> Created Time: 2014年11月15日 星期六 19时59分43秒
+
+	> Description: 
+ ************************************************************************/
+
+#include "pcl.h"
+
+// 打印日志
+unsigned char VarInfo;
+
+
+/* 错误处理函数 */
+void Error (Coord coord, const char *fmt, ...) 
+{
+    va_list ap;
+
+    ErrorCount++;
+    if (coord) {
+
+        fprintf (stderr, "(%s, %d):", coord->filename, coord->ppline);
+    }
+    fprintf (stderr, "error:");
+    
+    va_start (ap, fmt);
+    vfprintf (stderr, fmt, ap);
+    fprintf (stderr, "\n");
+    va_end (ap);
+}
+
+/* 致命的错误 */
+void Fatal (const char *fmt, ...)
+{
+    va_list ap;
+
+    va_start (ap, fmt);
+    vfprintf (stderr, fmt, ap);
+    fprintf (stderr, "\n");
+    va_end (ap);
+    /* 遇见此类错误直接退出 */
+    exit (-1);
+}
+
+void INFO (int infoLevel, const char *fmt, ...)
+{
+    if (VarInfo >= infoLevel) {
+
+        va_list ap;
+
+        va_start (ap, fmt);
+        fprintf (stdout, "INFO: ");
+        vfprintf (stdout, fmt, ap);
+        fprintf (stdout, "\n");
+        va_end (ap);
+    }
+}
+
+/* 警告处理 */
+void Warning (Coord coord, const char *fmt, ...) 
+{
+    va_list ap;
+
+    WarningCount++;
+    if (coord) {
+
+        fprintf (stderr, "(%s, %d):", coord->filename, coord->ppline);
+    }
+    fprintf (stderr, "warning:");
+    
+    va_start (ap, fmt);
+    vfprintf (stderr, fmt, ap);
+    fprintf (stderr, "\n");
+    va_end (ap);
+}
